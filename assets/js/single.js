ed80f1eb7e28c370682df848b68f2d3ef4c538b4
Paul Barcenas <paulbarcenas@yahoo.com>
2021-09-08 20:14:28 -0500

    create link between pages, read and use data from query parameter, and handle errors

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,23 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+
+    var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    
+    if(repoName) {
+        // display repo name on the page
+        getRepoIssues(repoName);
+    repoNameEl.textContent = repoName;
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+};
+
 var getRepoIssues = function(repo) {
   console.log(repo);
 
@@ -9,10 +28,16 @@ var getRepoIssues = function(repo) {
       if (response.ok) {
           response.json().then(function(data) {
               displayIssues(data);
+
+              // check if api has paginated issues
+              if (response.headers.get("Link")) {
+                  displayWarning(repo);
+              }
           });
       }
       else {
-          alert("There was a problem with your request!");
+          // if not successful, redirect to homepage
+          document.location.replace("./index.html");
       }
   });
 };
@@ -53,4 +78,17 @@ var displayIssues = function(issues) {
     }
    
 };
-getRepoIssues("facebook/react");
+
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl =document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
+getRepoName();
