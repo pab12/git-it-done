9aeef4bfa5deb8be99243306f3a63514c71c6928
Paul Barcenas <paulbarcenas@yahoo.com>
2021-09-08 21:07:22 -0500

    created language buttons that will let us see a list of featured repos that use that language

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,7 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
-
+var languageButtonsEl = document.querySelector("#language-buttons")
 
 var getUserRepos = function(user) {
     // format the github api url
@@ -83,4 +83,32 @@ var displayRepos = function(repos, searchTerm) {
         repoContainerEl.appendChild(repoEl);
     }
 }
+
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+   
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+               displayRepos(data.items, language);
+            });
+        } else {
+            alert('Error: GitHub User Not Found');
+        }
+    })
+};
+
+function buttonClickHandler(event){
+    var language = event.target.getAttribute("data-language");
+
+    if(language) {
+        getFeaturedRepos(language);
+
+        // clear old content
+        repoContainerEl.textContent = "";
+    }
+    console.log(language);
+}
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
